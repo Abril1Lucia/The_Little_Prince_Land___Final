2a56dbc54b9be7b84ad856607703676eb2b7ae6a
Abril1Lucia <abrilcastellanoslopez@gmail.com>
2025-02-06 23:26:04 -0500

    coneccion a la web

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,8 @@ import { usersRouter } from "./src/routes/Users.routes.js";
 import loginRouter from "./src/routes/login.routes.js";
 import { adminsRouter } from "./src/routes/Admins.routes.js";
 import cors from "cors";
-
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express(); 
 dotenv.config(); //se configura para poder usar variables we
@@ -16,12 +17,30 @@ const port = process.env.PORT;
 connectionMongo();
 app.use(cors())
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 app.use(express.json());
 app.use('/imagenes', productRouter);
 app.use('/usuarios', usersRouter)
 app.use('/iniciarSesion', loginRouter)
 app.use('/Admin', adminsRouter)
 
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// Ruta principal para servir index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+
 app.listen(port, () => {
   console.log("lo logramos :)", port);
 });
+
+
+// Exportar la aplicación sin iniciar el servidor
+export default app;
